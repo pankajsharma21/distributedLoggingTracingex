89f40a402f1d9f7fc1106395b2878759ddf604ca
PANKAJ SHARMA <gaur.ps21@outlook.com>
2023-02-11 18:27:56 +0000

    finsl

---
+++ src/main/java/com/microservice/distributedLoggingTracingex/DistributedLoggingTracingexApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.distributedLoggingTracingex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedLoggingTracingexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedLoggingTracingexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/distributedLoggingTracingex/DistributedLoggingTracingexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.distributedLoggingTracingex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedLoggingTracingexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
